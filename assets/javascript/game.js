a679efb5c146a32b79ff45e03dc43935b47480d8
Mia112 <msheriff112@gmail.com>
2019-06-21 20:12:15 -0400

    add to javascript and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,93 @@
 var generatedNumber;
-var lost;
-var win;
+var lost = 0;
+var win =0;
+var previous = 0;
 
 
-//create 4 crystals
-for(var i = 0; i < 4; i++) {
+var resetAndStart = function (){
 
-// add a random hidden value between 1 and 12 on each crystals
+    $(".crystals").empty();
+
+  
+    // capture our random number shown at the start of the game, between 19 - 120.
+    //the math here is: Math.floor(Math.random() * (max - min)) + min
+
+    generatedNumber = Math.floor(Math.random() * 101) + 19;
+    
+    // console.log(generatedNumber);
+    
+    //this is the element/area that displays the choosen number on the page
+    //$("#random-result").html('Choosen Number: ' + generatedNumber);
+       
+    $("#random-result").html(generatedNumber);
+
+    //create 4 crystals
+
+    for(var i = 0; i < 4; i++) {
+
+    // add a random hidden value between 1 and 12 on each crystals
     var randomNumber = Math.floor(Math.random() * 12) + 1;
        
-        // console.log(randomNumber);
+    // console.log(randomNumber);
 
-    var crystal = $("<div>");
+        var crystal = $("<div>");
         crystal.attr({
             "class": 'crystal',
             "num-value": randomNumber
         });
     
-    $(".crystals").append(crystal);
+        $(".crystals").append(crystal);
    
+    }
+
+    $("#previous").html("Your Total: " + previous);
 }
-// capture our random number shown at the start of the game, between 19 - 120.
-    //the math here is: Math.floor(Math.random() * (max - min)) + min
-    generatedNumber = Math.floor(Math.random() * 101) + 19;
 
-    // console.log(generatedNumber);
+    resetAndStart ();
 
-//this is the element/area that displays the choosen number on the page
-    $("#random-result").html('Choosen Number: ' + generatedNumber);
-
-//this allow us to click on each crystal 
-    $(".crystal").on('click', function() {
+    //this allow us to click on each crystal 
+    //event Delegation
+    
+    $(document).on('click', ".crystal", function() {
 
     // console.log($(this));
 
-//to get the value of each crystal that we click
-    $(this).attr('num-value');
+    //to get the value of each crystal that we click
+    
+    var number = parseInt($(this).attr('num-value'));
 
-    console.log($(this).attr('num-value'));
+    // console.log($(this).attr('num-value'));
+        
+    previous += number;
+
+    $("#previous").html("Total Score: " + previous);
+
+    console.log(previous);
+    
+    if(previous > generatedNumber) {
+        
+        lost++
+
+        $("#lost-count").html("You Lost: " + lost);
+        
+        previous = 0;
+
+        resetAndStart();
+
+        // console.log("You Lost! ");
+    }
+    else if(previous === generatedNumber) {
+        
+        win++;
+        
+        $("#win-count").html("You Win: " + win);
+            
+        previous = 0;
+
+
+        resetAndStart();
+      
+    }
+      
+
 });
